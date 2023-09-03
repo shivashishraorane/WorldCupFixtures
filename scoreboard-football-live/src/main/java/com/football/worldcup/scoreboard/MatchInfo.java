e3d1c36174fbf087a5cd33c23d99a53ea18acbad
Shivashish Rane <Shivashish.Rane@int.ccoe.cloud>
2023-09-03 18:57:51 +0200

    created Match Info to be used for score board

--- scoreboard-football-live/src/main/java/com/football/worldcup/scoreboard/MatchInfo.java
+++ scoreboard-football-live/src/main/java/com/football/worldcup/scoreboard/MatchInfo.java
@@ -1,6 +1,60 @@
 package com.football.worldcup.scoreboard;
 
 public class MatchInfo {
-    public MatchInfo(String homeTeam, String awayTeam, int homeTeamScore, int awayTeamScore, int gameSequence) {
+    private String homeTeam;
+    private String awayTeam;
+    private Integer homeTeamGoals;
+    private Integer awayTeamGoals;
+    private Integer gameSequence;
+
+    public MatchInfo(String homeTeam, String awayTeam, Integer homeTeamGoals, Integer awayTeamGoals,
+                        Integer gameSequence) {
+        super();
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+        this.homeTeamGoals = homeTeamGoals;
+        this.awayTeamGoals = awayTeamGoals;
+        this.gameSequence = gameSequence;
     }
+
+    public String getHomeTeam() {
+        return homeTeam;
+    }
+
+    public void setHomeTeam(String homeTeam) {
+        this.homeTeam = homeTeam;
+    }
+
+    public String getAwayTeam() {
+        return awayTeam;
+    }
+
+    public void setAwayTeam(String awayTeam) {
+        this.awayTeam = awayTeam;
+    }
+
+    public Integer getHomeTeamScore() {
+        return homeTeamGoals;
+    }
+
+    public void setHomeTeamScore(Integer homeTeamGoals) {
+        this.homeTeamGoals = homeTeamGoals;
+    }
+
+    public Integer getAwayTeamScore() {
+        return awayTeamGoals;
+    }
+
+    public void setAwayTeamScore(Integer awayTeamGoals) {
+        this.awayTeamGoals = awayTeamGoals;
+    }
+
+    public Integer getGameSequence() {
+        return gameSequence;
+    }
+
+    public void setGameSequence(Integer gameSequence) {
+        this.gameSequence = gameSequence;
+    }
+
 }
